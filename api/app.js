40b030be7d2d62e18d2facebd1d3d57239a33647
Darren Ross <darrenbross@gmail.com>
2023-02-19 16:46:30 -0800

    Starting Step 5 - Creating Lists and adding to sidebar.

--- api/app.js
+++ api/app.js
@@ -9,6 +9,12 @@ const { List, Task } = require('./db/models');
 // Middleware
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 /* Route Handlers*/
 
 /* List Routes */
